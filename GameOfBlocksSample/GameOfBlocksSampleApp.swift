40bd018846f60e049d5ffe4d41d9059afd0cd43e
Nicolò Curioni <nicolo.curioni96@gmail.com>
2022-11-23 13:58:50 +0100

    Initial Commit

---
+++ GameOfBlocksSample/GameOfBlocksSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfBlocksSampleApp.swift
+//  GameOfBlocksSample
+//
+//  Created by Nicolò Curioni on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct GameOfBlocksSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
