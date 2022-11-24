567536454b1216229248da436e9f911c8d9cc8b2
Nicolò Curioni <nicolo.curioni96@gmail.com>
2022-11-24 20:55:33 +0100

    Added the `GameView` view

---
+++ GameOfBlocksSample/Views/GameView.swift
@@ -0,0 +1,20 @@
+//
+//  GameView.swift
+//  GameOfBlocksSample
+//
+//  Created by Nicolò Curioni on 24/11/22.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text("Hello, Game of Blocks!")
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
