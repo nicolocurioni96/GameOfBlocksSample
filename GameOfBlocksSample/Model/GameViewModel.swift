9b6fadf9551550422e9ac2e660d99f0d27b05434
Nicolò Curioni <nicolo.curioni96@gmail.com>
2022-11-24 21:56:23 +0100

    Replaced old models with the `GameViewModel` model class

--- GameOfBlocksSample/Model/GameViewModel.swift
+++ GameOfBlocksSample/Model/GameViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  GameScoreData.swift
+//  GameViewModel.swift
 //  GameOfBlocksSample
 //
 //  Created by Nicolò Curioni on 24/11/22.
@@ -8,13 +8,17 @@
 import Foundation
 import SwiftUI
 
-struct GameScoreData {
+final class GameViewModel: ObservableObject {
     let emptyNeighborScore = 1
     let filledNeighborScore = 5
     let initialBlockScore = 5
     let numberOfColumns = 5
     let numberOfRows = 5
     
+    var selectedBlocks = [[String : Int]]()
+    var scoreArray = [[String : Int]]()
+    var finalScore = ""
+    var isUserInteractionDisabled = false
     var resultLength = 10
     var columns: [GridItem] {
         Array(repeating: .init(.adaptive(minimum: 60)), count: numberOfColumns)
